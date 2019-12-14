5b6ab48bb93a486c5d4b57ee66dc424461889573
SR Runblade <noreply@noreply>
2019-12-14 21:21:57 +0800

    Added blockchain prototype runner

---
+++ experiencer/pipeliner/CSharpBlockchain/CSharpBlockchain.cs
@@ -0,0 +1,135 @@
+using System;
+using System.Security.Cryptography;
+using System.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CSharpBlockchain
+{
+    public class Transaction
+    {
+        public string From      { get;  }
+        public string To        { get;  }
+        public double Amount    { get;  }    
+        //Constructor
+        public Transaction(string from, string to, double amount)
+        {
+            From    = from;
+            To      = to;
+            Amount  = amount;
+        }
+    }
+   
+    public class Block
+    {
+        private readonly DateTime _timeStamp;    
+        private long _nonce;    
+        public string PreviousHash              { get; set;         }
+        public List<Transaction> Transactions   { get; set;         }
+        public string Hash                      { get; private set; }
+    
+        public Block(DateTime timeStamp, List<Transaction> transactions, string previousHash = "")
+        {
+            _timeStamp = timeStamp;
+            _nonce = 0;        
+            Transactions = transactions;
+            PreviousHash = previousHash;
+            Hash = CreateHash();
+        }    
+        
+        public void MineBlock(int proofOfWorkDifficulty)
+        {
+            string hashValidationTemplate = new String('0', proofOfWorkDifficulty);
+            while (Hash.Substring(0, proofOfWorkDifficulty) != hashValidationTemplate)
+            {
+                _nonce++;
+                Hash = CreateHash();
+            }        
+            Console.WriteLine("Blocked with HASH={0} successfully mined!", Hash);
+        }    
+        
+        public string CreateHash()
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                string rawData = PreviousHash + _timeStamp + Transactions + _nonce;            
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(rawData));
+                return BitConverter.ToString(bytes).Replace("-","");
+            } 
+        }
+    }
+
+    public class BlockChain
+    {
+        private readonly int _proofOfWorkDifficulty;
+        private readonly double _miningReward;    
+        private List<Transaction> _pendingTransactions;    
+        public List<Block> Chain { get; set; }    
+        
+        public BlockChain(int proofOfWorkDifficulty, int miningReward)
+        {
+            _proofOfWorkDifficulty = proofOfWorkDifficulty;
+            _miningReward = miningReward;
+            _pendingTransactions = new List<Transaction>();
+            Chain = new List<Block> {CreateGenesisBlock()};
+        }    
+        
+        public void CreateTransaction(Transaction transaction)
+        {
+            _pendingTransactions.Add(transaction);
+        }    
+        
+        public void MineBlock(string minerAddress)
+        {
+            Transaction minerRewardTransaction = new Transaction(null, minerAddress, _miningReward);
+            _pendingTransactions.Add(minerRewardTransaction);        Block block = new Block(DateTime.Now, _pendingTransactions);
+            block.MineBlock(_proofOfWorkDifficulty);        
+            block.PreviousHash = Chain.Last().Hash;
+            Chain.Add(block);        
+            _pendingTransactions = new List<Transaction>();
+        }    
+        
+        public bool IsValidChain()
+        {
+            for (int i = 1; i < Chain.Count; i++)
+            {
+                Block previousBlock = Chain[i - 1];
+                Block currentBlock = Chain[i];            
+                if (currentBlock.Hash != currentBlock.CreateHash())
+                    return false;            
+                if (currentBlock.PreviousHash != previousBlock.Hash)
+                    return false;
+            }       
+            return true;
+        }    
+
+        public double GetBalance(string address)
+        {
+            double balance = 0;        
+            foreach (Block block in Chain)
+            {
+                foreach (Transaction transaction in block.Transactions)
+                {
+                    if (transaction.From == address)
+                    {
+                        balance -= transaction.Amount;
+                    }                
+                    if (transaction.To == address)
+                    {
+                        balance += transaction.Amount;
+                    }
+                }
+            }        
+            return balance;
+        }    
+        
+        private Block CreateGenesisBlock()
+        {
+            List<Transaction> transactions = new List<Transaction> {new Transaction("", "", 0)};
+            return new Block(DateTime.Now, transactions, "0");
+        }
+    }   
+}
+
+    
---
+++ experiencer/pipeliner/CSharpBlockchain/Program.cs
@@ -0,0 +1,62 @@
+﻿﻿using System;
+using System.Collections.Generic;
+
+namespace CSharpBlockchain
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Beginning blockchain run...");
+
+            //Set up mining and user addresses (can be modified for non-"coin" uses)            
+            const string minerAddress = "miner1";
+            const string user1Address = "A";
+            const string user2Address = "B";       
+
+            //Create the blockchain (genesis)
+            BlockChain blockChain = new BlockChain(proofOfWorkDifficulty: 5, miningReward: 10);
+            blockChain.CreateTransaction(new Transaction(user1Address, user2Address, 200));
+            blockChain.CreateTransaction(new Transaction(user2Address, user1Address, 10));
+            Console.WriteLine("Is valid: {0}", blockChain.IsValidChain());
+            
+            //Mining test 1
+            Console.WriteLine("--------- Start mining ---------");
+            blockChain.MineBlock(minerAddress);        
+            Console.WriteLine("BALANCE of the miner: {0}", blockChain.GetBalance(minerAddress));
+
+            //Mining test 2
+            blockChain.CreateTransaction(new Transaction(user1Address, user2Address, 5));        
+            Console.WriteLine("--------- Start mining ---------");
+            blockChain.MineBlock(minerAddress);        
+            Console.WriteLine("BALANCE of the miner: {0}", blockChain.GetBalance(minerAddress));        
+            
+            //Output entire blockchain
+            PrintChain(blockChain);       
+            
+            //Test hack of blockchain
+            Console.WriteLine("Hacking the blockchain...");
+            blockChain.Chain[1].Transactions = new List<Transaction> { new Transaction(user1Address, minerAddress, 150)};
+            Console.WriteLine("Is valid: {0}", blockChain.IsValidChain());        
+        }
+
+         private static void PrintChain(BlockChain blockChain)
+        {
+            Console.WriteLine("----------------- Start Blockchain -----------------");
+            foreach (Block block in blockChain.Chain)
+            {
+                Console.WriteLine("------ Start Block ------");
+                Console.WriteLine("Hash: {0}", block.Hash);
+                Console.WriteLine("Previous Hash: {0}", block.PreviousHash);            
+                Console.WriteLine("--- Start Transactions (In This Block) ---");
+                foreach (Transaction transaction in block.Transactions)
+                {
+                    Console.WriteLine("From: {0} To {1} Amount {2}", transaction.From, transaction.To, transaction.Amount);
+                }
+                Console.WriteLine("--- End Transactions (In This Block) ---");            
+                Console.WriteLine("------ End Block ------");
+            }
+            Console.WriteLine("----------------- End Blockchain -----------------");
+        }
+    }
+}
