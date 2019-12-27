d08dbffc7613bdbb30293f97e745593fa2e5207b
Runblade (AU) <noreply@noreply>
2019-12-27 10:42:55 +0800

    Improved docker commands

--- experiencer/pipeliner/CSharpBlockchain/commands.ps1
+++ experiencer/pipeliner/CSharpBlockchain/commands.ps1
@@ -1,9 +1,14 @@
 #Use this to build the docker image
 dotnet publish -c Release
-docker build -t rb-csharpblockchain -f Dockerfile .
+docker build -t rb-experiencer-csharpblockchain -f Dockerfile .
+
+#Use this to tag and push the docker image to docker hub
+docker tag rb-experiencer-csharpblockchain runblade/platform:rb-experiencer-csharpblockchain
+docker push runblade/platform:rb-experiencer-csharpblockchain
 
 #Use this to run the docker image (pass PoW difficulty as parameter)
-docker run -d --name lc-csharpblockchain rb-csharpblockchain 10
+#ALWAYS RUN FROM docker hub repository to avoid versioning issues!
+docker run -d --name lc-experiencer-csharpblockchain runblade/platform:rb-experiencer-csharpblockchain 10
 
-#Use this to view the output
-docker attach lc-csharpblockchain
+#Use this to view the output (lc prefix means local run)
+docker attach lc-experiencer-csharpblockchain
